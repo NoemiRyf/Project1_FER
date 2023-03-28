724a4c86dc98b3490b131faf20d2aa74e2983005
Noemi <ryfnoe01@students.zhaw.ch>
2023-03-28 18:08:40 +0200

    remove cv2

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from fer import FER
-import cv2
 from flask import Flask, url_for, render_template
 from flask.helpers import send_from_directory
 from flask_uploads import UploadSet, IMAGES, configure_uploads
@@ -7,8 +6,8 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 from werkzeug.utils import secure_filename
-# from werkzeug.datastructures import  FileStorage
-
+from PIL import Image
+import numpy as np
 
 
 app = Flask(__name__)
@@ -46,16 +45,16 @@ def upload_image():
     if form.validate_on_submit():
         filename = secure_filename(photos.save(form.photo.data))
         file_url = url_for('get_file', filename=filename)
-        input_image = cv2.imread("uploads/smile.jpg")
+        # Open image using PIL
+        input_image = Image.open("uploads/smile.jpg")
+        input_image_arr = np.array(input_image)
         emotion_detector = FER()
         # Output image's information
-        entry = (emotion_detector.detect_emotions(input_image))
-        print("entry")
+        entry = emotion_detector.detect_emotions(input_image_arr)
+        print(entry)
            
     else:
         file_url = None
     
     html = render_template('faceExpressionRecognition.html', form=form, file_url=file_url, entry=entry)
     return html
-     
-
