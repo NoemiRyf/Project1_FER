c035b2dc340a28bb6615b07f375d22f520f07e02
Noemi <ryfnoe01@students.zhaw.ch>
2023-04-02 15:30:19 +0200

    8000 port

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def welcome():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8080, debug=True)
+    app.run(host="localhost", port=8000, debug=True)
